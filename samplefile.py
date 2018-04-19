0eb0a91137fa8c99894db3f4f1a36ca778de891c
hvu815 <38333072+hvu815@users.noreply.github.com>
2018-04-19 14:44:29 -0700

    Update samplefile.py

--- samplefile.py
+++ samplefile.py
@@ -1,3 +1,5 @@
 #sample
 #https://www.kaggle.com/pmarcelino/comprehensive-data-exploration-with-python
 #https://www.kaggle.com/arthurtok/introduction-to-ensembling-stacking-in-python
+https://archive.ics.uci.edu/ml/datasets/Diabetes+130-US+hospitals+for+years+1999-2008
+  
