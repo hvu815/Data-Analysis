2f06a7a9f8c1aed1fbafbb722552a93eb0f8e08e
HUONG VU <huongvu@campus-003-122.ucdavis.edu>
2018-04-26 13:50:05 -0700

    huong

--- samplefile.py
+++ samplefile.py
@@ -1,5 +1,7 @@
 #sample
 #https://www.kaggle.com/pmarcelino/comprehensive-data-exploration-with-python
 #https://www.kaggle.com/arthurtok/introduction-to-ensembling-stacking-in-python
-https://archive.ics.uci.edu/ml/datasets/Diabetes+130-US+hospitals+for+years+1999-2008
+#https://archive.ics.uci.edu/ml/datasets/Diabetes+130-US+hospitals+for+years+1999-2008
   
+#https://www.hindawi.com/journals/bmri/2014/781670/tab1/
+
